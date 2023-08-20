d8557c8b52f20000910aa8c263b8c134aaee9a03
Gerald <code@gera2ld.space>
2023-08-20 12:45:47 +0800

    fix: build metadata in renderChunk stage

--- src/index.js
+++ src/index.js
@@ -1,81 +1,25 @@
 import { promises as fs } from 'fs';
-import { attachScopes } from '@rollup/pluginutils';
-import { walk } from 'estree-walker';
-import isReference from 'is-reference';
-
-const gmAPIs = [
-  'GM_info',
-  'GM_getValue',
-  'GM_setValue',
-  'GM_deleteValue',
-  'GM_listValues',
-  'GM_addValueChangeListener',
-  'GM_removeValueChangeListener',
-  'GM_getResourceText',
-  'GM_getResourceURL',
-  'GM_addStyle',
-  'GM_openInTab',
-  'GM_registerMenuCommand',
-  'GM_unregisterMenuCommand',
-  'GM_notification',
-  'GM_setClipboard',
-  'GM_xmlhttpRequest',
-  'GM_download',
-];
-const META_START = '// ==UserScript==';
-const META_END = '// ==/UserScript==';
+import { collectGmApi, getMetadata } from './util';
 
 export default (metafile, transform) => {
   const grantMap = new Map();
   return {
-    name: 'banner',
+    name: 'userscript-metadata',
     buildStart() {
       this.addWatchFile(metafile);
     },
     transform(code, id) {
       const ast = this.parse(code);
-      let scope = attachScopes(ast, 'scope');
-      const grantSetPerFile = new Set();
-      walk(ast, {
-        enter(node, parent) {
-          if (node.scope) scope = node.scope;
-          if (node.type === 'Identifier' && isReference(node, parent) && !scope.contains(node.name)) {
-            if (gmAPIs.includes(node.name)) {
-              grantSetPerFile.add(node.name);
-            }
-          }
-        },
-        leave(node) {
-          if (node.scope) scope = scope.parent;
-        },
-      });
+      const grantSetPerFile = collectGmApi(ast);
       grantMap.set(id, grantSetPerFile);
     },
-    async banner() {
-      let meta = await fs.readFile(metafile, 'utf8');
-      const lines = meta.split('\n').map(line => line.trim());
-      const start = lines.indexOf(META_START);
-      const end = lines.indexOf(META_END);
-      if (start < 0 || end < 0) {
-        console.warn('Invalid metadata block. For more details see https://violentmonkey.github.io/api/metadata-block/');
-        return;
-      }
+    /**
+     * Use `renderChunk` instead of `banner` to preserve the metadata after minimization.
+     * Note that this plugin must be put after `@rollup/plugin-terser`.
+     */
+    async renderChunk(code) {
+      const meta = await fs.readFile(metafile, 'utf8');
       const grantSet = new Set();
-      const items = lines.slice(start + 1, end)
-        .map(line => {
-          if (!line.startsWith('// ')) return;
-          line = line.slice(3).trim();
-          const i = line.indexOf(' ');
-          if (i < 0) return;
-          const key = line.slice(0, i);
-          const value = line.slice(i + 1).trim();
-          if (key === '@grant') {
-            grantSet.add(value);
-            return;
-          }
-          return [key, value];
-        })
-        .filter(Boolean);
       for (const id of this.getModuleIds()) {
         const grantSetPerFile = grantMap.get(id);
         if (grantSetPerFile) {
@@ -84,20 +28,9 @@ export default (metafile, transform) => {
           }
         }
       }
-      const grantList = Array.from(grantSet);
-      grantList.sort();
-      for (const item of grantList) {
-        items.push(['@grant', item]);
-      }
-      const maxKeyWidth = Math.max(...items.map(([key]) => key.length));
-      meta = [
-        META_START,
-        ...items.map(([key, value]) => `// ${key.padEnd(maxKeyWidth)} ${value}`),
-        META_END,
-        '',
-      ].join('\n');
-      if (transform) meta = transform(meta);
-      return meta;
+      let metadata = getMetadata(meta, grantSet);
+      if (transform) metadata = transform(metadata);
+      return `${metadata}\n\n${code}`;
     },
   };
 };
---
+++ src/util.js
@@ -0,0 +1,83 @@
+import { attachScopes } from '@rollup/pluginutils';
+import { walk } from 'estree-walker';
+import isReference from 'is-reference';
+
+const gmAPIs = [
+  'GM_info',
+  'GM_getValue',
+  'GM_setValue',
+  'GM_deleteValue',
+  'GM_listValues',
+  'GM_addValueChangeListener',
+  'GM_removeValueChangeListener',
+  'GM_getResourceText',
+  'GM_getResourceURL',
+  'GM_addStyle',
+  'GM_openInTab',
+  'GM_registerMenuCommand',
+  'GM_unregisterMenuCommand',
+  'GM_notification',
+  'GM_setClipboard',
+  'GM_xmlhttpRequest',
+  'GM_download',
+];
+const META_START = '// ==UserScript==';
+const META_END = '// ==/UserScript==';
+
+export function collectGmApi(ast) {
+  let scope = attachScopes(ast, 'scope');
+  const grantSetPerFile = new Set();
+  walk(ast, {
+    enter(node, parent) {
+      if (node.scope) scope = node.scope;
+      if (node.type === 'Identifier' && isReference(node, parent) && !scope.contains(node.name)) {
+        if (gmAPIs.includes(node.name)) {
+          grantSetPerFile.add(node.name);
+        }
+      }
+    },
+    leave(node) {
+      if (node.scope) scope = scope.parent;
+    },
+  });
+  return grantSetPerFile;
+}
+
+export function getMetadata(metaFileContent, additionalGrantList) {
+  const lines = metaFileContent.split('\n').map(line => line.trim());
+  const start = lines.indexOf(META_START);
+  const end = lines.indexOf(META_END);
+  if (start < 0 || end < 0) {
+    throw new Error('Invalid metadata block. For more details see https://violentmonkey.github.io/api/metadata-block/');
+  }
+  const grantSet = new Set();
+  const entries = lines.slice(start + 1, end)
+    .map(line => {
+      if (!line.startsWith('// ')) return;
+      line = line.slice(3).trim();
+      const i = line.indexOf(' ');
+      if (i < 0) return;
+      const key = line.slice(0, i);
+      const value = line.slice(i + 1).trim();
+      if (key === '@grant') {
+        grantSet.add(value);
+        return;
+      }
+      return [key, value];
+    })
+    .filter(Boolean);
+  for (const item of additionalGrantList) {
+    grantSet.add(item);
+  }
+  const grantList = Array.from(grantSet);
+  grantList.sort();
+  for (const item of grantList) {
+    entries.push(['@grant', item]);
+  }
+  const maxKeyWidth = Math.max(...entries.map(([key]) => key.length));
+  return [
+    META_START,
+    ...entries.map(([key, value]) => `// ${key.padEnd(maxKeyWidth)} ${value}`),
+    META_END,
+  ].join('\n');
+}

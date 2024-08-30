16e6eb13a5fc2cc93e296e93ecc5a7b97305f4f1
Gerald <code@gera2ld.space>
2024-08-30 14:41:29 +0800

    fix: add missing GM_* APIs

--- src/util.ts
+++ src/util.ts
@@ -1,18 +1,22 @@
 import { AttachedScope, attachScopes } from '@rollup/pluginutils';
-import type { AstNode } from 'rollup';
-import { walk, Node } from 'estree-walker';
+import { Node, walk } from 'estree-walker';
 import isReference from 'is-reference';
+import type { AstNode } from 'rollup';
 
 const gmAPIs = [
   'GM_info',
   'GM_getValue',
+  'GM_getValues',
   'GM_setValue',
+  'GM_setValues',
   'GM_deleteValue',
+  'GM_deleteValues',
   'GM_listValues',
   'GM_addValueChangeListener',
   'GM_removeValueChangeListener',
   'GM_getResourceText',
   'GM_getResourceURL',
+  'GM_addElement',
   'GM_addStyle',
   'GM_openInTab',
   'GM_registerMenuCommand',
@@ -31,7 +35,11 @@ export function collectGmApi(ast: AstNode) {
   walk(ast as Node, {
     enter(node: Node & { scope: AttachedScope }, parent) {
       if (node.scope) scope = node.scope;
-      if (node.type === 'Identifier' && isReference(node, parent) && !scope.contains(node.name)) {
+      if (
+        node.type === 'Identifier' &&
+        isReference(node, parent) &&
+        !scope.contains(node.name)
+      ) {
         if (gmAPIs.includes(node.name)) {
           grantSetPerFile.add(node.name);
         }
@@ -44,16 +52,22 @@ export function collectGmApi(ast: AstNode) {
   return grantSetPerFile;
 }
 
-export function getMetadata(metaFileContent: string, additionalGrantList: Set<string>) {
-  const lines = metaFileContent.split('\n').map(line => line.trim());
+export function getMetadata(
+  metaFileContent: string,
+  additionalGrantList: Set<string>,
+) {
+  const lines = metaFileContent.split('\n').map((line) => line.trim());
   const start = lines.indexOf(META_START);
   const end = lines.indexOf(META_END);
   if (start < 0 || end < 0) {
-    throw new Error('Invalid metadata block. For more details see https://violentmonkey.github.io/api/metadata-block/');
+    throw new Error(
+      'Invalid metadata block. For more details see https://violentmonkey.github.io/api/metadata-block/',
+    );
   }
   const grantSet = new Set<string>();
-  const entries = lines.slice(start + 1, end)
-    .map(line => {
+  const entries = lines
+    .slice(start + 1, end)
+    .map((line) => {
       if (!line.startsWith('// ')) return;
       line = line.slice(3).trim();
       const matches = line.match(/^(\S+)(\s.*)?$/);
